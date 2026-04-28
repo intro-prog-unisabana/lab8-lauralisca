30a0caef08a79e007a5faa2560187b65810cf8e8
Laura Lisca <lauralisgo@unisabana.edu.co>
2026-04-28 18:54:00 -0500

    lab 8

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 """Laboratorio 8 - CLI del gestor de tareas."""
+import sys
+from todo_manager import read_todo_file, write_todo_file
 
-# TODO: Implementar CLI según README.md
+if len(sys.argv) < 2:
+        raise IndexError("Insufficient arguments provided!")
+
+    
+if sys.argv[1] == "--help":
+        print("""Usage: python main.py <file_path> <command> [arguments]...
+
+Commands:
+  add "task"    - Add a task to the list.
+  remove "task" - Remove a task from the list.
+  view          - Display all tasks.
+
+Examples:
+  python main.py tasks.txt add "Buy groceries"
+  python main.py tasks.txt remove "Do laundry"
+  python main.py tasks.txt view
+  python main.py tasks.txt add "Call mom" remove "Take out trash" view""")
+        sys.exit()
+
